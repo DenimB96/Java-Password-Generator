40c5be92acb6d1b55e1c87b57f49536d879ccc50
DenimB96 <rlsjr2@live.com>
2023-01-11 12:46:17 -0500

    Password generator is functional with a minimum of 8 charcters and symbols

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 8;
+    var password = "";
+
+for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+}
+    document.querySelector("#password").value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+/* New password code */ 
+
+
+
+
+
